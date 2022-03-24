9dfb2b94e59f527e5b047cc6ee6eb46a74b28e69
Dillon J. Choi <dillonchoi1992@gmail.com>
2022-03-24 15:09:41 -0400

    added serach function and jokes list

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using JokesMVCapp.Models;
 
 namespace JokesMVCapp.Data
 {
@@ -9,5 +10,6 @@ namespace JokesMVCapp.Data
             : base(options)
         {
         }
+        public DbSet<JokesMVCapp.Models.Joke> Joke { get; set; }
     }
 }
